e4a4eb7b0d6a095b34e970f10fe9c78e08c7dbdf
JBSantos <jbsantos.dev@gmail.com>
2025-04-03 22:21:41 -0300

    Close Proposal

--- src/EnhencedBallot.sol
+++ src/EnhencedBallot.sol
@@ -15,6 +15,7 @@ contract EnhencedBallot {
         address creator;
         uint256 id;
         uint256 votes;
+        bool isActive;
     }
 
     mapping(uint256 ballotId => Proposal) public registry;
@@ -31,12 +32,25 @@ contract EnhencedBallot {
         hasVotedForProposalID[_proposalID][msg.sender] = true;
     }
 
+    function closeProposal(uint256 _proposalID) public {
+        require(
+            registry[_proposalID].creator == msg.sender,
+            "You need to be the creator to close a proposal"
+        );
+        require(
+            registry[_proposalID].isActive == true,
+            "Proposal Already Inactive"
+        );
+        registry[_proposalID].isActive = false;
+    }
+
     function createNewProposal(string memory _name) public {
         registry[ballotsCounter] = Proposal({
             name: _name,
             creator: msg.sender,
             id: ballotsCounter,
-            votes: 0
+            votes: 0,
+            isActive: true
         });
 
         emit ProposalCreation(_name, msg.sender, ballotsCounter);
